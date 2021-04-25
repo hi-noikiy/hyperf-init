7bb8ba0c35b3ed56568c394106d98a65895971a8
noikiy <noikiy@live.com>
2021-04-25 12:39:45 +0800

    引入队列

---
+++ app/Factory/JobFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Factory;
+
+use Hyperf\AsyncQueue\Driver\DriverFactory;
+use Hyperf\AsyncQueue\Driver\DriverInterface;
+
+/**
+ * Class JobFactory
+ * @noinspection PhpUnused
+ * @package App\Factory
+ */
+class JobFactory
+{
+    /**
+     * @var DriverInterface
+     */
+    protected $driver;
+
+    public function __construct(DriverFactory $driverFactory)
+    {
+        $this->driver = $driverFactory->get('default');
+    }
+
+    /**
+     * 插入队列
+     * @param $job
+     * @param int $delay
+     * @return bool
+     */
+    public function push($job, $delay = 0): bool
+    {
+        return $this->driver->push($job, $delay);
+    }
+}
